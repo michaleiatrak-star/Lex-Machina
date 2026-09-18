b88b73c7c4a0e666bbd2f21c2cef63694bcb3661
Michał <judoslaw@wp.pl>
2026-09-18 23:29:08 +0200

    Pin canonical artwork to repository bytes verified by CI

--- app/installer/materialize-brand-icon.ps1
+++ app/installer/materialize-brand-icon.ps1
@@ -8,7 +8,7 @@ param(
 )
 
 $ErrorActionPreference = "Stop"
-$expectedSourceSha256 = "d2dff5cc49b4a8e3e0a9f484f7c14ad80f17e89c28abce44f6ec9c00d7270701"
+$expectedSourceSha256 = "1cb02043e7ae64df0b2cb04270b7211bba8ab4238e5ca8bfad9c50fb2caadaf3"
 $sizes = @(16, 24, 32, 48, 64, 128, 256)
 
 function Get-Sha256Hex([byte[]]$Bytes) {
