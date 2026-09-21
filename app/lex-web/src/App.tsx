d84669b3d7a0213b3d6299f73eddb72f45bdd2f1
Michał <judoslaw@wp.pl>
2026-09-21 23:12:38 +0200

    refactor(0.1.7): compose account and system settings in app hub

--- app/lex-web/src/App.tsx
+++ app/lex-web/src/App.tsx
@@ -1,18 +1,65 @@
-import MatterChatApp from "./MatterChatApp.js";
+import MatterChatApp, {
+  type SettingsRequest
+} from "./MatterChatApp.js";
 import { LocalAiSetupPanel } from "./LocalAiSetupPanel.js";
 import { MaintenancePanel } from "./MaintenancePanel.js";
-import type { AuthenticatedUser } from "./api.js";
+import { AccountSecurityPanel } from "./AccountSecurityPanel.js";
+import { AdminUsersPanel } from "./AdminUsersPanel.js";
+import { AdminSupportPanel } from "./AdminSupportPanel.js";
+import type {
+  AuthMeResponse,
+  AuthenticatedUser
+} from "./api.js";
 
 export default function App({
-  user
+  user,
+  onAuthUpdated,
+  settingsRequest
 }: {
   user: AuthenticatedUser;
+  onAuthUpdated: (
+    value: AuthMeResponse
+  ) => void;
+  settingsRequest?: SettingsRequest | null;
 }) {
   return (
-    <>
-      <MatterChatApp user={user} />
-      <MaintenancePanel user={user} />
-      <LocalAiSetupPanel user={user} />
-    </>
+    <MatterChatApp
+      user={user}
+      settingsRequest={settingsRequest}
+      settingsPanels={{
+        localAi: (
+          <LocalAiSetupPanel
+            user={user}
+            embedded
+          />
+        ),
+        security: (
+          <AccountSecurityPanel
+            user={user}
+            onAuthUpdated={
+              onAuthUpdated
+            }
+          />
+        ),
+        users:
+          user.appRole ===
+          "ADMIN" ? (
+            <>
+              <AdminUsersPanel
+                currentUserId={
+                  user.userId
+                }
+              />
+              <AdminSupportPanel />
+            </>
+          ) : null,
+        maintenance: (
+          <MaintenancePanel
+            user={user}
+            embedded
+          />
+        )
+      }}
+    />
   );
 }
